31e44139a8359dd9d35b15067aafcad9ed416f88
shwetagogi23 <145103384+shwetagogi23@users.noreply.github.com>
2023-09-16 15:16:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,4 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.multiselect("Pick some Fruits", list(my_fruit_list.index)
 
 #Display the table on the page
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
