addbe682d361cc97ad9f843eb9c65c9421d3b6b3
shwetagogi23 <145103384+shwetagogi23@users.noreply.github.com>
2023-09-16 15:04:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 
-[global]
 dataFrameSerialization = "legacy"
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
